bcf6faf4932d87544e77d6f745d3d8aff6184f00
oierpalacios-del <oier.palacios@opendeusto.es>
2026-04-17 08:33:58 +0200

    cubo de robots

--- Assets/scripts/spawner.cs
+++ Assets/scripts/spawner.cs
@@ -3,11 +3,20 @@ using UnityEngine;
 public class spawner : MonoBehaviour
 {
     public Object prefab;
-    void Start()
+    private void Start()
     {
-        Instantiate(prefab);
+        for (int i = 0; i < 100; i++)
+        {
+            Vector3 pos = transform.position;
+            pos.x = pos.x * i;
+            for(int j = 0; j<100; j++)
+            {
+                pos.z = transform.position.z;
+                pos.z = pos.z * j;
+                Instantiate(prefab, pos, Quaternion.identity);
+            }
+        }
     }
-
     // Update is called once per frame
     void Update()
     {
