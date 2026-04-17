c70b1fb22641714b32babb95eedc8a06a36e2a61
oierpalacios-del <oier.palacios@opendeusto.es>
2026-04-17 08:51:37 +0200

    movement mejorado

--- Assets/scripts/movement.cs
+++ Assets/scripts/movement.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 
 public class movement : MonoBehaviour
 {
+    public float speed;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,23 +12,23 @@ public class movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.D))
+        if (Input.GetKey(KeyCode.D))
         {
-            this.transform.position = new Vector3(gameObject.transform.position.x + 1, gameObject.transform.position.y, gameObject.transform.position.z);
+            this.transform.position = new Vector3(gameObject.transform.position.x + 1 * speed * Time.deltaTime, gameObject.transform.position.y, gameObject.transform.position.z);
         }
 
-        if (Input.GetKeyDown(KeyCode.A))
+        if (Input.GetKey(KeyCode.A))
         {
-            this.transform.position = new Vector3(gameObject.transform.position.x - 1, gameObject.transform.position.y, gameObject.transform.position.z);
+            this.transform.position = new Vector3(gameObject.transform.position.x - 1 * speed * Time.deltaTime, gameObject.transform.position.y, gameObject.transform.position.z);
         }
-        if (Input.GetKeyDown(KeyCode.W))
+        if (Input.GetKey(KeyCode.W))
         {
-            this.transform.position = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y, gameObject.transform.position.z + 1);
+            this.transform.position = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y, gameObject.transform.position.z + 1 * speed * Time.deltaTime);
         }
 
-        if (Input.GetKeyDown(KeyCode.S))
+        if (Input.GetKey(KeyCode.S))
         {
-            this.transform.position = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y, gameObject.transform.position.z - 1);
+            this.transform.position = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y, gameObject.transform.position.z - 1 * speed * Time.deltaTime);
         }
     }
 }
