6e8d448fb6f880786221ee86ac2490d9d39e8366
oierpalacios-del <oier.palacios@opendeusto.es>
2026-04-23 09:03:57 +0200

    layermask detection

--- Assets/scripts/piu.cs
+++ Assets/scripts/piu.cs
@@ -7,10 +7,24 @@ public class piu : MonoBehaviour
     {
         if (Input.GetMouseButtonDown(0))
         {
-            Debug.DrawRay(transform.position, transform.forward,Color.green);
             GameObject instance = Instantiate(prefab);
             instance.transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z + 2);
             instance.GetComponent<Rigidbody>().AddForce(transform.forward*5000,ForceMode.Force);
+        }
+    }
+    private void FixedUpdate()
+    {
+        int layerMask = 1<<6;
+        RaycastHit hit;
+        if(Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, Mathf.Infinity, layerMask))
+        {
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.red);
+            Debug.Log("Did hit " + hit.collider.gameObject.name);
+        }
+        else
+        {
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * 1000, Color.white);
+            Debug.Log("Did not hit");
         }
     }
 }
